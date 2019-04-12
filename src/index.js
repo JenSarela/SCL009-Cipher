958e300b6f3a875ff8e1934cb591a65fb8f4cbc8
JenSarela <jen.sarela24@gmail.com>
2019-04-12 08:43:18 -0400

    Subiendo cambios a HTML,css y JS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
-//offset 
+//offset permite al usuario seleccionar sus desplazamientos
 let offset = document.getElementById("offset").value;   
 
 //obtener el mensaje a codificar
-let saveTexto = document.getElementById("text").value;
+/*let saveTexto = document.getElementById("text").value;*/
 
-//llamar al boton para codificar y decodificar usando un evento
+// llama al boton para obtener y guardar el nombre y mostralo en la pantalla
 document.getElementById("nick_name").addEventListener("click", () => {
 let user = document.getElementById("user").value;
 document.getElementById("nick").innerHTML = `Hola ${user}`
@@ -15,8 +15,7 @@ document.getElementById("category_codificar").addEventListener("click", () => {
 
     let text = document.getElementById("text").value;
     let offset = parseInt(document.getElementById("offset").value);
-    //let resultado = document.getElementById("answer").value;
-    //if (text != ""){
+ 
       let finalResult = cipher.encode(offset,text);
       document.getElementById("answer").innerHTML = `${finalResult}`
     //aqui se deberia mostrar al usuario su nuevo codigo
